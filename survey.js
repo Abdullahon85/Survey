3b4372896c14676ce864619fea6ac90db7f0dc1b
Abdullakhon <Avazbekovabdullahon85@gmail.com>
2023-03-03 17:06:03 +0500

    last

--- survey.js
+++ survey.js
@@ -5,10 +5,8 @@ var munus = prompt("Решите пример 13-4=? ")
 var delen = prompt("Решите пример 16/2=? ")
 var umno = prompt("Решите пример 13*3=? ")
 var ost = prompt("Решите пример 13%2=? ")
-var one = prompt( "Введите первое число")
-var two = prompt( "Введите второе число")
-var three = prompt( "Введите третье число")
- 
+
+
 console.log("Меня завут " + ame );
 console.log('Мне ' + word + " лет");
 console.log("Пример1; 13+8= " + (13 + 8) + " Ваш ответ: " + plus );
@@ -16,4 +14,10 @@ console.log("Пример1; 13-4= " + (13 - 4) + " Ваш ответ: " + munus 
 console.log("Пример1; 16/2=" + (16 / 2) + " Ваш ответ: " + delen );
 console.log("Пример1; 13*2= " + (13 * 2) + " Ваш ответ: " + umno );
 console.log("Пример1; 13%2= " + (13 % 2) + " Ваш ответ: " + ost );
-alert( "Среднее арифметическое: " + ( (one + two + three) / 3) ) 
+var X = +prompt( "Введите первое число")
+console.log();
+var Y = +prompt( "Введите второе число")
+console.log();
+var Z = +prompt( "Введите третье число")
+console.log();
+alert ((X+Y+Z) / 3)
